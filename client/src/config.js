e29666d4848bacd4342c08dfd63a4cbcf9ca2cae
Siddarthan S P <siddarthan@outlook.com>
2018-03-03 22:37:14 +0530

    update ips

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	apiBaseUrl: 'http://localhost:7000',
-	socketBaseUrl: 'http://localhost:4001',
+	apiBaseUrl: 'http://159.89.175.10:7000', 
+	socketBaseUrl: 'http://159.89.175.10:4001',
 }
