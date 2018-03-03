a8f1bef2cb8e169c08da271c4caa705d968ec9ca
Siddarthan S P <siddarthan@outlook.com>
2018-03-03 20:20:50 +0530

    Move item added event from client to post end point

--- apis/routeHandlers/todoPostHandler.js
+++ apis/routeHandlers/todoPostHandler.js
@@ -1,12 +1,16 @@
 
 const TodoItem = require('../models/TodoItem.js');
-
+const  socketIOClient= require('socket.io-client');
+const config = require('../../config.js');
+const socket = socketIOClient(config.socketBaseUrl);
 //End point to post an item
 module.exports = async (req, res) => {
 	const { name } = req.body;
 	const todoItem = new TodoItem({name});
 	const item = await todoItem.save();
+    socket.emit('item added', item); 
 	res.send({
     	item: item
-    });
+	});
+	
 };
