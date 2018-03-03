03b0a92797987ff7ee17382cc667cd6d242e2983
Siddarthan S P <siddarthan@outlook.com>
2018-03-04 01:29:10 +0530

    Revert "update ips"
    This reverts commit e29666d4848bacd4342c08dfd63a4cbcf9ca2cae.

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	apiBaseUrl: 'http://159.89.175.10:7000', 
-	socketBaseUrl: 'http://159.89.175.10:4001',
+	apiBaseUrl: 'http://localhost:7000',
+	socketBaseUrl: 'http://localhost:4001',
 }
--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	mongoUrl: "mongodb://159.89.175.10:27017/stupidtodo-db",
-	apiBaseUrl: "http://159.89.175.10:7000",
-	socketBaseUrl: "http://159.89.175.10:4001"
+	apiBaseUrl: "http://localhost:7000",
+	socketBaseUrl: "http://localhost:4001"
 };
